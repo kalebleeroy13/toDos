013dd2b45d1e7415e840f10820190bbd33899255
khuunter@yahoo.com <khuunter@yahoo.com>
2020-05-22 17:37:48 -0400

    refactored variable and function firstLi to lis

--- todos.js
+++ todos.js
@@ -1,5 +1,13 @@
-let firstLi = document.querySelector("li");
+let lis = document.querySelectorAll("li");
 
-firstLi.addEventListener("mouseover", function(){
-  firstLi.style.color = "green";
-});
+for(let i = 0; i < lis.length; i++){
+ lis[i].addEventListener("mouseover", function(){
+   this.style.color = "green";
+ });
+ lis[i].addEventListener("mouseout", function(){
+   this.style.color = "black";
+ });
+lis[i].addEventListener("click", function(){
+  this.classList.toggle("done"):
+})
+};
