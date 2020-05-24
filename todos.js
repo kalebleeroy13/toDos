6f7bf4caf558253f2218fa0f27f129221994e706
khuunter@yahoo.com <khuunter@yahoo.com>
2020-05-24 17:12:31 -0400

    new variable list to refactor code using event delegation

--- todos.js
+++ todos.js
@@ -1,13 +1,28 @@
-let lis = document.querySelectorAll("li");
+// let lis = document.querySelectorAll("li");
+//
+// for(let i = 0; i < lis.length; i++){
+//  lis[i].addEventListener("mouseover", function(){
+//    this.style.color = "red";
+//  });
+//  lis[i].addEventListener("mouseout", function(){
+//    this.style.color = "black";
+//  });
+// lis[i].addEventListener("click", function(){
+//   this.classList.toggle("done");
+// })
+// }
 
-for(let i = 0; i < lis.length; i++){
- lis[i].addEventListener("mouseover", function(){
-   this.style.color = "green";
- });
- lis[i].addEventListener("mouseout", function(){
-   this.style.color = "black";
- });
-lis[i].addEventListener("click", function(){
-  this.classList.toggle("done");
-})
-}
+let list = document.querySelector('ul');
+
+list.addEventListener('mouseover', function(event){
+		//conditional here
+		list.style.color = "red"
+	})
+    list.addEventListener('mouseout', function(event){
+		//conditional here
+		list.style.color = "black";
+	})
+    list.addEventListener('click', function(event){
+		//conditional here
+		list.classList.toggle("done");
+	});
